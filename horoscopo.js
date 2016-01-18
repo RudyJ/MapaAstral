59db475c39e6a5264a3da450e887ee86da9b1d9c
mspavanelli <mspavanelli@gmail.com>
2016-01-18 03:17:44 -0200

    v0.1
    - criação de tabela dinamicamente por jQuery
    - listagem de signos
    - adiciona aspecto (bug)

--- horoscopo.js
+++ horoscopo.js
@@ -14,37 +14,85 @@ var signos = [
 	'Peixes'
 ]
 
+var aspectos = [
+	'Lua',
+	'Mercúrio',
+	'Vênus',
+	'Marte',
+	'Júpiter',
+	'Saturno',
+	'Urano',
+	'Netuno',
+	'Plutão',
+	'Nódulo Lunar'
+]
+
 /* FUNCOES */
 function listaSignos( signoBase ) {
-	var lista = [];
-	for ( var i = signos.indexOf(signoBase); i < signos.length; i++ )
-		lista.push(signos[i]);
-	for ( var i = 0; i < signos.indexOf(signoBase); i++ )
-		lista.push(signos[i]);
-	return lista;
+	while ( signos[0] != signoBase )
+		signos.unshift(signos.pop());
+	return signos;
 }
 
 /* jQuery */
 
-$('.label').hide();
-$('.panel').hide();
+var insereOpcoesdeSignos = function() {
+	$.each(signos, function(chave, valor) {
+		$('<option>').attr('value', valor).text(valor).appendTo($('.seletor-signo'));
+	});
+}
 
-$('#run').click(function() {
-	// console.log( $('#signoBase').val() );
-	var signo = $('#signoBase').val();
+var montaAspectos = function() {
+	$.each(aspectos, function(chave, valor) {
+		$('<option>').attr('value', valor).text(valor).appendTo($('.seletor-aspecto '));
+	});
+}
 
-	var signos = listaSignos(signo);
-	if ( signo != "") {
-		for( var i = 1; i <= 12; i++ )
-			$('#' + i + ' .signo').text(signos[i-1]);
-		$('.label').show();
-		$('.panel').show();
+var preencheLinha = function(signos) {
+ 	var i = 0;
+ 	$('.signo').each(function() {
+ 		$(this).text(signos[i]);
+ 		i++;
+ 	});
+}
+
+var montaTabela = function() {
+	
+	var tabela = $('<table>').attr('id', 'tabela-signo').addClass('table table-striped table-hover');
+	
+	var head = $('<thead>').appendTo(tabela);
+	$('<th>').text('Casas').appendTo(head);
+	$('<th>').text('Signo').appendTo(head);
+	$('<th>').text('Aspectos').appendTo(head);
+
+	var body = $('<tbody>').appendTo(tabela);
+	for ( var i = 0; i < 12; i++ ) {
+		var linha = $('<tr>').appendTo(body);
+		if ( i == 0 ) linha.addClass('info'); // refatorar
+		$('<td>').text(i+1).appendTo(linha);
+		$('<td>').appendTo(linha).addClass('signo');
+		$('<td>').appendTo(linha).addClass('aspecto');
 	}
-	else {
-		$('.label').hide();
-		$('.panel').hide();
-		for( var i = 1; i <= 12; i++ )
-			$('#' + i + ' .signo').text("");	
-	}
 
+	tabela.appendTo($('#dados'));
+}
+
+var adicionaAspecto = function() {
+	var signo = $(".seletor-signo option:selected").val();
+	var aspecto = $(".seletor-aspecto option:selected").val();
+	console.log('Inserindo ' + aspecto + ' em ' + signo);
+}
+
+$(document).ready( function() {
+	$(insereOpcoesdeSignos());
+	$(montaTabela());
+	$(montaAspectos());
+	
+	$('#run').click(function() {
+		var signo = $(".seletor-signo option:selected").val();
+		$(preencheLinha(listaSignos(signo)));
+		$('select').removeAttr('disabled');
+	});
+	
+	$('#adiciona-aspecto').click(adicionaAspecto);
 });
