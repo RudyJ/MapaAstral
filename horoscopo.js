c437f785333e09d5e3d9548851d24516e5fcd4da
mspavanelli <mspavanelli@gmail.com>
2016-01-25 00:44:41 -0200

    v0.3
    - inclui análise do nome (template)
    - gera relatório com signos individuais
    - refatoração

--- horoscopo.js
+++ horoscopo.js
@@ -34,21 +34,19 @@ function listaSignos( signoBase ) {
 	return signos;
 }
 
-/* jQuery */
-
-var insereOpcoesdeSignos = function() {
+function insereOpcoesdeSignos() {
 	$.each(signos, function(chave, valor) {
 		$('<option>').attr('value', valor).text(valor).appendTo($('.seletor-signo'));
 	});
 }
 
-var montaAspectos = function() {
+function montaAspectos() {
 	$.each(aspectos, function(chave, valor) {
 		$('<option>').attr('value', valor).text(valor).appendTo($('.seletor-aspecto '));
 	});
 }
 
-var preencheLinha = function(signos) {
+function preencheLinha(signos) {
  	var i = 0;
  	$('.signo').each(function() {
  		$(this).text(signos[i]);
@@ -56,14 +54,14 @@ var preencheLinha = function(signos) {
  	});
 }
 
-var montaTabela = function() {
-	
-	var tabela = $('<table>').attr('id', 'tabela-signo').addClass('table table-striped table-hover table-responsive');
-	
+function montaTabela() {
+
+	var tabela = $('<table>').attr('id', 'tabela-signo').addClass('table table-striped table-hover table-responsive text-center');
+
 	var head = $('<thead>').appendTo(tabela);
-	$('<th>').text('Casas').appendTo(head);
-	$('<th>').text('Signo').appendTo(head);
-	$('<th>').text('Aspectos').appendTo(head);
+	$('<th>').addClass('col-xs-2 text-center').text('Casas').appendTo(head);
+	$('<th>').addClass('col-xs-4 text-center').text('Signo').appendTo(head);
+	$('<th>').addClass('col-xs-6 text-center').text('Aspectos').appendTo(head);
 
 	var body = $('<tbody>').appendTo(tabela);
 	for ( var i = 0; i < 12; i++ ) {
@@ -77,7 +75,7 @@ var montaTabela = function() {
 	tabela.appendTo($('#dados'));
 }
 
-var adicionaAspecto = function() {
+function adicionaAspecto() {
 	var signo = $(".seletor-signo.aspecto option:selected").val();
 	var aspecto = $(".seletor-aspecto option:selected").val();
 
@@ -87,21 +85,49 @@ var adicionaAspecto = function() {
 	rotulo.appendTo(celula);
 }
 
-var inserePainel = function(signo, aspecto) {
-	console.log('Inserindo painel (' + signo + ':' + aspecto + ')');
+function getText() {
+	return " Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+}
 
+function significadoLetra(letra) {
+	return 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut pa qui offi'
+}
+
+function inserePainel(local, signo, aspecto) {
 	// código painel + inserir após resultado
+	var titulo;
+	if ( aspecto != null ) {
+		titulo = aspecto + ' em ' + signo;
+	}
+	else {
+		titulo = signo;
+	}
 	var painel = $('<div>').addClass('panel panel-default')
-		.appendTo($('<div>').addClass('col-xs-12 col-md-6').appendTo('#resultados'));
-	$('<h3>').addClass('panel-title').text(aspecto + ' em ' + signo)
+		.appendTo($('<div>').addClass('col-xs-12 col-md-6').appendTo(local));
+	$('<h3>').addClass('panel-title').text(titulo)
 		.appendTo($('<div>').addClass('panel-heading')
 			.appendTo(painel));
-	$('<div>').addClass('panel-body').text('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus voluptate labore ab dignissimos, dolore sed accusamus. Nulla quasi illum assumenda inventore quae odit eos repellat vero? Dolores architecto assumenda minus.')
+	$('<div>').addClass('panel-body').text(getText())
 		.appendTo(painel);
 }
 
-var geraResultado = function() {
-	var i = 0;
+function getAspectos() {
+	var aspectos = [];
+	$('td.aspecto').each(function() {
+		$(this).children().each(function() {
+			aspectos.push($(this));
+		});
+	});
+	return aspectos;
+}
+
+function incluirPainelSignos() {
+	$('.signo').each(function() {
+		inserePainel('#resultado-signos',$(this).text(), null);
+	});
+}
+
+function incluirPainelAspectos() {
 	var aspectos = [];
 	$('.signo').each(function() {
 		var signo = $(this).text();
@@ -109,14 +135,39 @@ var geraResultado = function() {
 			aspectos.push(($(this).text()));
 		});
 
-		for (var i = aspectos.length - 1; i >= 0; i--) {
-			inserePainel(signo, aspectos[i]);
-		};
+		for (var i = 0; i < aspectos.length; i++) {
+			inserePainel('#resultado-aspectos',signo, aspectos[i]);
+		}
 		aspectos = [];
 	});
 }
 
-var testeAutomatizado = function() {
+function incluirAnaliseNome() {
+	var letras = $('#nome').val().split("");
+
+	for ( var i = 0; i < letras.length; i++ ) {
+		var linha = $('<tr>');
+		$('<th>').addClass('col-xs-1').text(letras[i]).appendTo(linha);
+		$('<td>').addClass('col-xs11').text(significadoLetra(letras[i])).appendTo(linha);
+		linha.appendTo($('#analise-nome > tbody'));
+	}
+}
+
+function geraResultado() {
+	incluirPainelSignos();
+	incluirPainelAspectos();
+	incluirAnaliseNome();
+}
+
+function deletaAspectos() {
+	$('td.aspecto').each(function() {
+		$(this).children().each(function() {
+			$(this).remove();
+		});
+	});
+}
+
+function testeAutomatizado() {
 
 	// seleciona signo
 	$(preencheLinha(listaSignos('Áries')));
@@ -133,23 +184,23 @@ $(document).ready( function() {
 	$(montaAspectos());
 
 	$('form').attr('onsubmit', 'return false;'); // previne que a página seja recarregada
-	
+
 	$('#run').click(function() {
 		var signo = $(".seletor-signo option:selected").val();
 		$(preencheLinha(listaSignos(signo)));
 		$('select').removeAttr('disabled');
+		deletaAspectos();
 	});
 
 	$('#adiciona-aspecto').click(adicionaAspecto);
-	// remove aspecto
-	$('.label').on('click', function() {
-		$(this).animate({'fontSize':'100px', 'margin-left':'+=100'}, function() {
-			// $(this).remove();
-		});
+
+	// TODO: remover aspecto específico
+	$('label, .label.label-default').dblclick(function() {
+		$(this).css('background-color','transparent');
+		// $(this).remove();
 	});
 
 	$('#gera-resultado').click(geraResultado);
 
 	$(testeAutomatizado());
 });
-
